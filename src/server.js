31f092c5f3934cb0aebdaa906e8a5d6b7a2bbc90
Eduardos13 <edik.kozelsky@gmail.com>
2024-09-15 15:27:54 -0400

    Update server.js

--- src/server.js
+++ src/server.js
@@ -35,18 +35,31 @@ export const setupServer = () => {
   app.get('/contacts', async (req, res) => {
     const contacts = await getAllContacts();
     res.status(200).json({
-      contacts,
+      status: 200,
+      message: 'Successfully found contacts!',
+      data: contacts,
     });
   });
 
   app.get('/contacts/:contactId', async (req, res) => {
-    const { contactId } = req.params;
-
-    const contact = await getContactById(contactId);
-
-    res.status(200).json({
-      contact,
-    });
+    try {
+      const { contactId } = req.params;
+      const contact = await getContactById(contactId);
+      if (!contact) {
+        res.status(404).json({
+          message: 'Contact not found',
+        });
+      }
+      res.status(200).json({
+        status: 200,
+        message: `Successfully found contact with id ${contactId}!`,
+        data: contact,
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: 'Internal server error',
+      });
+    }
   });
 
   app.use(notFoundMiddleware);
