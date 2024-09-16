24c4125f1e8ad052c28d569950e79a52e99ac537
Eduardos13 <edik.kozelsky@gmail.com>
2024-09-16 18:16:40 -0400

    fixed server
    added return to '/contacts/:contactId' to prevent server error after 'contact not found'

--- src/server.js
+++ src/server.js
@@ -46,17 +46,17 @@ export const setupServer = () => {
       const { contactId } = req.params;
       const contact = await getContactById(contactId);
       if (!contact) {
-        res.status(404).json({
+        return res.status(404).json({
           message: 'Contact not found',
         });
       }
-      res.status(200).json({
+      return res.status(200).json({
         status: 200,
         message: `Successfully found contact with id ${contactId}!`,
         data: contact,
       });
     } catch (error) {
-      res.status(500).json({
+      return res.status(500).json({
         message: 'Internal server error',
       });
     }
