702726b14d4b221ffc12b3b709e8d52fdbdc7f0d
Eduardos13 <edik.kozelsky@gmail.com>
2024-09-16 20:26:59 -0400

    first 2 steps are done

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,7 +1,18 @@
+import { HttpError } from 'http-errors';
+
 const errorHandlerMiddleware = (err, req, res, next) => {
+  if (err instanceof HttpError) {
+    res.status(err.status).json({
+      status: err.status,
+      message: err.message,
+      data: err,
+    });
+    return;
+  }
   res.status(500).json({
-    message: 'Something wrong',
-    error: err.message,
+    status: 500,
+    message: 'Something went wrong',
+    data: err.message,
   });
 };
 
