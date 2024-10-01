1e43f4bd964e84b9108b209ff368dd640f344ad0
Eduardos13 <edik.kozelsky@gmail.com>
2024-09-30 20:47:37 -0400

    session is done

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,6 +1,9 @@
 import bcrypt from 'bcrypt';
+import { randomBytes } from 'crypto';
 import { UserCollection } from '../db/models/user.js';
+import { SessionCollection } from '../db/models/session.js';
 import createHttpError from 'http-errors';
+import { FIFTEEN_MINUTES, ONE_DAY } from '../constants/index.js';
 
 export const registerUser = async (payload) => {
   const user = await UserCollection.findOne({ email: payload.email });
@@ -19,8 +22,22 @@ export const loginUser = async (payload) => {
   if (!user) {
     throw createHttpError(404, 'User not found');
   }
+
   const isEqual = await bcrypt.compare(payload.password, user.password);
   if (!isEqual) {
     throw createHttpError(401, 'Unauthorized');
   }
+
+  await SessionCollection.deleteOne({ userId: user._id });
+
+  const accessToken = randomBytes(30).toString('base64');
+  const refreshToken = randomBytes(30).toString('base64');
+
+  return await SessionCollection.create({
+    userId: user._id,
+    accessToken,
+    refreshToken,
+    accessTokenValidUntil: new Date(Date.now() + FIFTEEN_MINUTES),
+    refreshTokenValidUntil: new Date(Date.now() + ONE_DAY),
+  });
 };
